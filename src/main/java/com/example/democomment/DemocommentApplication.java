09a454578f6b562c16f9c93a3f058ae8287d0e48
houfei <331488738@qq.com>
2019-05-13 21:52:28 +0800

    创建Spring项目

---
+++ src/main/java/com/example/democomment/DemocommentApplication.java
@@ -0,0 +1,13 @@
+package com.example.democomment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocommentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemocommentApplication.class, args);
+    }
+
+}
